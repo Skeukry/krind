b67a360855d117e1c20ed5e0d85e4881e3a6e59b
Ferenc Kálmán <ferenckalman1997@gmail.com>
2017-06-03 18:00:23 +0200

    Listen on PORT env variable

--- server/main.js
+++ server/main.js
@@ -4,14 +4,13 @@ const app = express();
 
 // Setup --------------------------------------------------
 app.disable('case sensitive routing');
-app.set('json spaces', 4);
 app.disable('strict routing');
+app.set('json spaces', 4);
 app.locals.fileOpts = {
     root: process.cwd() + '/client/'
 };
-app.locals.prod = () => app.get('env') === 'production';
 
-app.listen(app.locals.prod() ? 80 : 3000);
+app.listen(process.env.PORT || 3000);
 
 
 // Routing ------------------------------------------------
